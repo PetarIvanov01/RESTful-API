f598d596b72cb5067b0724f705c5f3a53675287e
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-26 17:25:25 +0300

    Set up MongoDB connection using Mongoose
    Implemented a function to establish a connection to the MongoDB database using Mongoose. The app connects to the database using the MONGO_URI environment variable, and it logs the successful connection with the host details.

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,3 +1,15 @@
-module.exports = (app) => {
-    return
+const mongoose = require('mongoose');
+
+module.exports = async (app) => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDb connected: ${conn.connection.host}`);
+
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 }
