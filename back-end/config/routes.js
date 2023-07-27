174f4bfee7fcf047f4f74d09953a533e97704030
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-27 14:23:01 +0300

    Add user routes to the app configuration
    Updated the app configuration to include user routes in addition to goal routes. Now, the application uses '/api/user' for user-related routes and '/api/goals' for goal-related routes. Also, added the error handler middleware to handle errors across the app.

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -1,8 +1,11 @@
 const goalRoute = require('../routes/goalRoute');
+const authRoute = require('../routes/userRoute');
+
 const { errroHandler } = require("../middleware/error");
 
 module.exports = (app) => {
 
+    app.use('/api/user', authRoute);
     app.use('/api/goals', goalRoute);
     app.use(errroHandler);
 
