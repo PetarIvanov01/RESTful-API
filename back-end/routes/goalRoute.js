9f25842220f2bcf57eff2d129f020b9050e7431a
Petar Ivanov <111183840+PetarIvanov01@users.noreply.github.com>
2023-07-27 14:21:21 +0300

    Add authorization middleware to goal routes

--- back-end/routes/goalRoute.js
+++ back-end/routes/goalRoute.js
@@ -1,11 +1,11 @@
 const { getGoals, createGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
-
+const authorization = require('../middleware/auth');
 const router = require('express').Router();
 
 // GET and POST methods mounted on the root path '/'
-router.route('/').get(getGoals).post(createGoal);
+router.route('/').get(authorization, getGoals).post(authorization, createGoal);
 
 // PUT and DELETE methods mounted on the path '/:id', where :id is the goal ID
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route('/:id').put(authorization, updateGoal).delete(authorization, deleteGoal);
 
 module.exports = router;
