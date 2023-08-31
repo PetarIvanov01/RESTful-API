49406f84a66130cb58dfe3e111c13e0f59223d02
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-31 12:40:44 +0300

    Refactoring

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -5,7 +5,10 @@ const { errroHandler } = require("../middleware/error");
 
 module.exports = (app) => {
 
+    //TODO Refactor user auth for my project purposes
     app.use('/api/user', authRoute);
+
+    //TODO Refactor Goal Route for my project purposes
     app.use('/api/goals', goalRoute);
     app.use(errroHandler);
 
