c5553c7d5b7645dfc86dd380454409815450b931
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-31 12:47:54 +0300

    Update

--- back-end/services/userService.js
+++ back-end/services/userService.js
@@ -1,56 +1,49 @@
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler')
 
-async function register({ email, password }) {
-    try {
+const register = asyncHandler(async ({ email, password }) => {
 
-        const exsiting = await User.findOne({ email });
-        if (email == '' || password == '') {
-            throw new Error('All fields are required!');
-        }
-
-        if (exsiting) {
-            throw new Error('Email is taken!');
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            email,
-            password: hashedPassword
-        });
-
-        const token = createSession(user._id, user.email);
-
-        return { user, token };
-
-    } catch (error) {
-        throw error
-    }
-}
-
-async function login({ email, password }) {
-    try {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            throw new Error('Incorrect username or password');
-        }
-        const hasMatch = await bcrypt.compare(password, user.password);
-
-        if (hasMatch == false) {
-            throw new Error('Incorrect username or password');
-        }
-
-        const token = createSession(user._id, user.email);
-
-        return { user, token };
-
-    } catch (error) {
-        throw error
+    const exsiting = await User.findOne({ email });
+    if (email == '' || password == '') {
+        throw new Error('All fields are required!');
     }
 
-}
+    if (exsiting) {
+        throw new Error('Email is taken!');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        email,
+        password: hashedPassword
+    });
+
+    const token = createSession(user._id, user.email);
+
+    return { user, token };
+
+})
+
+const login = asyncHandler(async ({ email, password }) => {
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Incorrect username or password');
+    }
+    const hasMatch = await bcrypt.compare(password, user.password);
+
+    if (hasMatch == false) {
+        throw new Error('Incorrect username or password');
+    }
+
+    const token = createSession(user._id, user.email);
+
+    return { user, token };
+
+})
 
 function createSession(_id, email) {
     const payload = {
@@ -71,5 +64,5 @@ function verifyToken(token) {
 module.exports = {
     login,
     register,
-    verifyToken
+    verifyToken,
 }
