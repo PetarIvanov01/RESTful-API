75909b990dbeedc44a8a7d52451dde454cc0df4c
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-08-31 18:06:20 +0300

    Remove middleware from get methods

--- back-end/routes/goalRoute.js
+++ back-end/routes/goalRoute.js
@@ -1,11 +1,11 @@
-const { getGoals, createGoal, updateGoal, deleteGoal } = require('../controllers/goalController');
+const { getGoalsById, createGoal, updateGoal, deleteGoal, getAllGoals } = require('../controllers/goalController');
 const authorization = require('../middleware/auth');
 const router = require('express').Router();
 
 // GET and POST methods mounted on the root path '/'
-router.route('/').get(authorization, getGoals).post(authorization, createGoal);
+router.route('/').get(getAllGoals).post(authorization, createGoal);
 
 // PUT and DELETE methods mounted on the path '/:id', where :id is the goal ID
-router.route('/:id').put(authorization, updateGoal).delete(authorization, deleteGoal);
+router.route('/:id').get(getGoalsById).put(authorization, updateGoal).delete(authorization, deleteGoal);
 
 module.exports = router;
