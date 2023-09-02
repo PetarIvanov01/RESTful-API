8d61c30e1735b52aec27223c2b040be4979492ee
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-09-02 14:30:53 +0300

    Update

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -4,7 +4,7 @@ const authorization = (req, res, next) => {
     try {
 
         const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(" ")[1];
+        const token = authHeader && authHeader.split(" ")[1] || authHeader;
 
         if (token) {
             const decode = verifyToken(token);
