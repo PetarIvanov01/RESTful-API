e10c567cdddf7293c5f82f30a5cdb858febdee4f
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-01 19:27:01 +0200

    Expanded user profile functionality to include follow and unfollow actions

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,5 +1,5 @@
 const { loginHandler, registerHandler } = require('../controllers/authController');
-const { createProfileHandler, getProfileHandler, editProfileHandler } = require('../controllers/profileController')
+const { createProfileHandler, getProfileHandler, editProfileHandler, followProfileHandler, unFollowProfileHandler } = require('../controllers/profileController')
 const authorization = require('../middleware/auth');
 
 const router = require('express').Router();
@@ -10,6 +10,9 @@ router.post('/login', loginHandler);
 router.post('/register', registerHandler);
 
 router.post('/profile', authorization, createProfileHandler);
+
+router.put('/profile/follow', authorization, followProfileHandler);
+router.put('/profile/un-follow', authorization, unFollowProfileHandler);
 
 router.route('/profile/:userId')
     .get(getProfileHandler)
