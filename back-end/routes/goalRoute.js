294d383a08732e7222c7fdf9521826fcb27431d9
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-07 21:41:54 +0200

    Updated Goal Routes and Added 'getHomeGoals' Route

--- back-end/routes/goalRoute.js
+++ back-end/routes/goalRoute.js
@@ -1,19 +1,20 @@
-const { getGoalsById, createGoal, updateGoal, deleteGoal, getAllGoals, getBySearch } = require('../controllers/goalController');
+const { getGoalsById, createGoal, updateGoal, deleteGoal, getAllGoals, getBySearch, getHomeGoals } = require('../controllers/goalController');
 const authorization = require('../middleware/auth');
 const paginatedResults = require('../middleware/pagination');
 const searchResult = require('../middleware/searchResult');
 const Profile = require('../model/UserProfile');
 const router = require('express').Router();
 
-// GET and POST methods mounted on the root path '/'
 router.route('/')
     .get(searchResult(Profile),paginatedResults(Profile), getAllGoals)
     .post(authorization, createGoal);
 
+router.route('/home')
+    .get(getHomeGoals)
+
 router.route('/search')
     .get(searchResult(Profile),getBySearch)
 
-// PUT and DELETE methods mounted on the path '/:id', where :id is the goal ID
 router.route('/:id')
     .get(getGoalsById).put(authorization, updateGoal)
     .delete(authorization, deleteGoal);
