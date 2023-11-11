db31dc82e7d654ed162cf7e80dab1e0568b185de
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-11 22:02:37 +0200

    Refactor routes in goal API for simplicity and remove unused middleware.

--- back-end/routes/goalRoute.js
+++ back-end/routes/goalRoute.js
@@ -1,19 +1,13 @@
 const { getGoalsById, createGoal, updateGoal, deleteGoal, getAllGoals, getBySearch, getHomeGoals } = require('../controllers/goalController');
 const authorization = require('../middleware/auth');
-const paginatedResults = require('../middleware/pagination');
-const searchResult = require('../middleware/searchResult');
-const Profile = require('../model/UserProfile');
 const router = require('express').Router();
 
 router.route('/')
-    .get(searchResult(Profile),paginatedResults(Profile), getAllGoals)
+    .get(getAllGoals)
     .post(authorization, createGoal);
 
 router.route('/home')
     .get(getHomeGoals)
-
-router.route('/search')
-    .get(searchResult(Profile),getBySearch)
 
 router.route('/:id')
     .get(getGoalsById).put(authorization, updateGoal)
