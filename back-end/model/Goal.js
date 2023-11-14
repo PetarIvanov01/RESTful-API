37672ef1e3899d8d020e5efa4c35d7d674194a84
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-14 14:25:43 +0200

    Improved goal schema validation and added error messages.
    Added validators for title length, description word count, and image URL format
    Improved error messages for better clarity on validation failures"

--- back-end/model/Goal.js
+++ back-end/model/Goal.js
@@ -1,25 +1,62 @@
 const { Schema, model } = require('mongoose');
 
+const wordCountValidatorExceed = (value) => {
+    const words = value.split(/\s+/).filter(word => word.length > 0);
+    return words.length <= 500;
+};
 
-const goalSchema = Schema({
-    title: {
-        type: String,
-        required: true
+const wordCountValidatorAtLeast = (value) => {
+    const words = value.split(/\s+/).filter(word => word.length > 0);
+    return words.length >= 20;
+};
+
+const validImageUrlValidator = (value) => {
+    const urlRegex = /^(https?:\/\/)([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+
+    return urlRegex.test(value) && /\.(jpg|jpeg|png|gif)$/i.test(value);
+};
+
+const goalSchema = Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required'],
+            minlength: [3, 'Title should be at least 3 characters long'],
+            maxlength: [50, 'Title cannot exceed 50 characters'],
+        },
+        description: {
+            type: String,
+            required: [true, 'Description is required'],
+            validate:
+                [
+                    {
+                        validator: wordCountValidatorExceed,
+                        message: 'Description cannot exceed 500 words',
+                    },
+                    {
+                        validator: wordCountValidatorAtLeast,
+                        message: 'Description should have at least 20 words',
+                    },
+                ],
+        },
+        image: {
+            type: String,
+            required: [true, 'Image URL is required'],
+            validate: [
+                {
+                    validator: validImageUrlValidator,
+                    message: 'Invalid image URL. It should start with http or https and end with a valid image extension (jpg, jpeg, png, gif)',
+                },
+            ],
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            required: [true, 'Owner ID is required'],
+        },
     },
-    description: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        required: true,
+    {
+        timestamps: true,
     }
-}, {
-    timestamps: true
-})
+);
 
 module.exports = model('Goal', goalSchema);
