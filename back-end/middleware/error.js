3317305ccfe9f19a8583b4c2e3fd28f28853ae40
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-14 14:24:08 +0200

    Enhanced error handling to extract mongoose custom errors and retrieving them. - is this good

--- back-end/middleware/error.js
+++ back-end/middleware/error.js
@@ -1,20 +1,18 @@
 const errroHandler = (err, req, res, next) => {
-    const typeError = err.type;
-
+    const typeError = err.name;
     let statusCode = res.statusCode ? res.statusCode : 500;
+    let errors = err;
+    let stack = err.stack
 
-    if (typeError === 'custom') {
-        statusCode = 403;
-        res.status(403);
-    }
-    else {
+    if (typeError === 'ValidationError') {
+        errors = Object.values(err.errors).map(err => err.message);
         statusCode = 400;
         res.status(400)
     }
 
     res.status(statusCode).json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+        message: Array.isArray(errors) ? errors : errors.message,
+        stack: process.env.NODE_ENV === 'production' ? null : stack
     })
 }
 
