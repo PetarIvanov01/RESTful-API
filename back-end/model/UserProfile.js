5be9e2662bde2109941d3ad7e6df2f7f8b1064b9
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-15 23:26:13 +0200

    "Refactor user profile schema for improved validation and avatar handling

--- back-end/model/UserProfile.js
+++ back-end/model/UserProfile.js
@@ -1,36 +1,63 @@
 const { Schema, model } = require('mongoose')
 
+const predefinedAvatars = require('../../MockDataPreload/MockData/mockAvatars.json')
+
+const validImageUrlValidator = (value) => {
+    const matchingAvatar = predefinedAvatars.some(
+        (predefinedAvatar) =>  predefinedAvatar.value === value
+    );
+    return matchingAvatar;
+};
+
+const validAvatarValidator = (label) => {
+    const matchingAvatar = predefinedAvatars.some(
+        (predefinedAvatar) => predefinedAvatar.label === label
+    );
+    return matchingAvatar;
+};
 
 const userProfileSchema = Schema({
-    userId:
-    {
+    userId: {
         type: Schema.Types.ObjectId,
-        required: true
+        required: [true, 'User ID is required'],
     },
-    username:
-    {
+    username: {
         type: String,
-        required: true
+        required: [true, 'Username is required'],
+        minlength: [3, 'Username should be at least 3 characters long'],
+        maxlength: [50, 'Username cannot exceed 50 characters'],
     },
     avatarImg: {
         value: {
             type: String,
-            required: true,
+            required: [true, 'Avatar image URL is required'],
+            validate: [
+                {
+                    validator: validImageUrlValidator,
+                    message: 'Invalid avatar image URL. It should start with http or https',
+                },
+            ],
         },
         label: {
             type: String,
-            required: true
-        }
+            required: [true, 'Avatar image label is required'],
+            validate: [
+                {
+                    validator: validAvatarValidator,
+                    message: 'Invalid avatar image. The combination of value and label should exist in the predefined avatars.',
+                },
+            ],
+        },
     },
-    category:
-    {
+    category: {
         type: String,
-        required: true
+        required: [true, 'Category is required'],
+        enum: ['sport', 'lifestyle', 'career'],
     },
-    aboutMe:
-    {
+    aboutMe: {
         type: String,
-        required: true
+        required: [true, 'About me is required'],
+        maxlength: [250, 'About me cannot exceed 250 characters'],
     },
     followers: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
     following: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
@@ -40,5 +67,6 @@ const userProfileSchema = Schema({
     timestamps: true
 })
 
-
 module.exports = model('Profile', userProfileSchema);
+
+
