c3898547353b7b0a9a57841e61a94f965c8d6890
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-20 17:14:27 +0200

    Update error

--- back-end/middleware/error.js
+++ back-end/middleware/error.js
@@ -1,17 +1,16 @@
 const errroHandler = (err, req, res, next) => {
     const typeError = err.name;
-    let statusCode = res.statusCode ? res.statusCode : 500;
     let errors = err;
     let stack = err.stack
 
-    if (typeError === 'ValidationError') {
-        errors = Object.values(err.errors).map(err => err.message);
-        statusCode = 400;
-        res.status(400)
-    }
+    let message = errors.message
 
-    res.status(statusCode).json({
-        message: Array.isArray(errors) ? errors : errors.message,
+    if (typeError === 'ValidationError') {
+        errors = Object.values(err.errors).map(err => err.message).join(', ');
+        message = errors;
+    }
+    res.status(400).json({
+        message,
         stack: process.env.NODE_ENV === 'production' ? null : stack
     })
 }
