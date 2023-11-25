38e4a907079338121ae1cf97b57d39a17d4fcfe3
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-11-25 16:46:29 +0200

    Enhance error handling in errorHandler
    - Added handling for 'TokenExpiredError' with an appropriate status code and message.

--- back-end/middleware/error.js
+++ back-end/middleware/error.js
@@ -1,7 +1,8 @@
 const errroHandler = (err, req, res, next) => {
     const typeError = err.name;
+    let status = 400;
     let errors = err;
-    let stack = err.stack
+    let stack = err.stack;
 
     let message = errors.message
 
@@ -9,7 +10,12 @@ const errroHandler = (err, req, res, next) => {
         errors = Object.values(err.errors).map(err => err.message).join(', ');
         message = errors;
     }
-    res.status(400).json({
+    if (typeError === 'TokenExpiredError') {
+        message = 'JWT Token has expired. Please log in again to obtain a new token.'
+        status = 401;
+    }
+    res.status(status).json({
+        type: typeError,
         message,
         stack: process.env.NODE_ENV === 'production' ? null : stack
     })
