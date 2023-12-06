fa2dfc7be1b6fab99c7af66382c473d3432e4a42
Firstname Lastname <111183840+PetarIvanov01@users.noreply.github.com>
2023-12-06 23:37:25 +0200

    refactor: Update commentSchema with additional constraints

--- back-end/model/Comments.js
+++ back-end/model/Comments.js
@@ -4,7 +4,9 @@ const commentSchema = new Schema(
     {
         message: {
             type: String,
-            required: true
+            required: true,
+            maxlength: [50, 'The comment should not exceed 50 symbols!'],
+            minlength: [5, 'The comment should be at least 5 symbols!']
         },
         userId: {
             type: Schema.Types.ObjectId,
@@ -21,6 +23,12 @@ const commentSchema = new Schema(
             ref: 'Comment',
             default: null
         },
+        depth: {
+            type: Number,
+            default: 1,
+            min: [1, 'The minimum depth is 1.'],
+            max: [2, 'The maximum depth is 2.'],
+        },
         children: [{
             type: Schema.Types.ObjectId,
             ref: 'Comment',
@@ -34,3 +42,5 @@ const commentSchema = new Schema(
 
 const Comment = model('Comment', commentSchema);
 module.exports = Comment;
+
+
